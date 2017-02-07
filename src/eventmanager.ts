80a003feef42fa15c235a13908112858ed93cb47
ransomware <mohamed.tahrioui@wheregroup.com>
2017-02-07 15:04:47 +0100

    Add eventmanager

---
+++ src/eventmanager.ts
@@ -0,0 +1,45 @@
+declare const $: any;
+
+export class EventManager {
+
+    private static eventMap = {};
+
+    private static addToEventMap(selector, eventType, handler) {
+        if (!EventManager.eventMap[eventType]) {
+            EventManager.eventMap[eventType] = {};
+        }
+        EventManager.eventMap[eventType][selector] = handler;
+
+        return handler;
+    }
+
+    private static removeFromEventMap(selector, eventType) {
+        if (!EventManager.eventMap[eventType]) {
+            EventManager.eventMap[eventType] = {};
+            return false;
+        }
+
+        let wasFound = !!EventManager.eventMap[eventType][selector];
+        EventManager.eventMap[eventType][selector] = undefined;
+        delete EventManager.eventMap[eventType][selector];
+
+        return wasFound;
+    }
+
+    public static registerOnKeypress(selector, key, handler) {
+        $(selector).keypress(function (event) {
+            if (event.which === key) {
+                event.preventDefault();
+                return handler(event);
+            }
+        });
+    }
+
+    public static unregisterOnKeypress(selector) {
+        let eventType = "keypress";
+        $(selector).removeAttr(eventType);
+        EventManager.removeFromEventMap(selector,eventType);
+    }
+    
+
+}

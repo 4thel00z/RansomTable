9d47740346297e469fe572ed8fcbb760322cc9c9
ransomware <mohamed.tahrioui@wheregroup.com>
2017-02-10 15:27:29 +0100

    Fix editmode

--- src/EditField.ts
+++ src/EditField.ts
@@ -7,12 +7,12 @@ export class EditField {
 
     private inputField: any;
 
-    constructor(content: string = "") {
-        this.load(content);
+    constructor(cell: Cell) {
+        this.load(cell);
     }
 
-    private load(content: string = "") {
-        this.inputField = $("<input>").addClass(Table.classes.input).val(content);
+    private load(cell: Cell) {
+        this.inputField = $("<input>").addClass(Table.classes.input).val(cell.element.text());
     }
 
     public get() {
@@ -24,7 +24,7 @@ export class EditField {
     }
 
     public edit(cell: Cell, event: Event) {
-       
+
         if (cell.readOnly) {
             return;
         }
@@ -33,23 +33,63 @@ export class EditField {
             return;
         }
         else {
-            this.enable(cell);
+            this.enable(cell, event);
+            const container = cell.table.container;
+            if (container) {
+                container.on("mousedown." + cell.element.attr("id"), {
+                    self: this,
+                    cell: cell
+                }, this.disable);
+            }
         }
     }
 
-    public disable(cell: Cell) {
-        let text: string = this.inputField.val();
+    public disable(event) {
+        const cell: Cell = event.data.cell;
+        const self: EditField = event.data.self;
+        if (EditField.inBounds(cell, event)) {
+            // Clicking on the same cell has to be ignored
+            return true;
+        }
+        let text: string = self.inputField.val();
         cell.element.empty();
         cell.element.text(text);
         cell.editMode = false;
+        const container = cell.table.container;
+        if (container) {
+            container.off("mousedown." + cell.element.attr("id"));
+        }
+        let readOnlyCells = cell.table.tableBody.find("." + Table.classes.readOnly);
+        return false;
+    }
+
+    public static inBounds(cell: Cell, event) {
+
+        const clientX: number = event.clientX;
+        const clientY: number = event.clientY;
+        const pageX: number = event.pageX;
+        const pageY: number = event.pageY;
+
+
+        const element = cell.element;
+        const width: number = element.outerWidth();
+        const height: number = element.outerHeight();
+        const offset: {top: number, left: number} = element.offset();
+
+        return clientX >= offset.left && clientX <= offset.left + width &&
+            clientY >= offset.top && clientY <= offset.top + height;
 
     }
 
-    public enable(cell: Cell) {
+    public enable(cell: Cell, event: Event) {
+
+        const self: EditField = this;
         cell.cache = cell.element.text();
         cell.element.empty();
         cell.element.append(this.inputField.val(cell.cache));
         cell.editMode = true;
+
+        return false;
     }
 
 }

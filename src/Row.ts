a2e5ebe905ed95b95a5792e90a276a9978723201
ransomware <mohamed.tahrioui@wheregroup.com>
2017-02-14 17:25:29 +0100

    Improve encapsulation. Clean up unnecessary methods

--- src/Row.ts
+++ src/Row.ts
@@ -1,70 +1,85 @@
 import {Cell} from "./Cell";
-import {CellElement, SimpleCellElement, ICellElement} from "./CellElement";
 import {RowElement} from "./RowElement";
 import {ElementType} from "./ElementType";
 import {Table} from "./Table";
-declare const $: any;
+import "jquery";
 
 export class Row {
 
-    protected element: any;
-    protected type: ElementType;
-    protected cells: Array<Cell>;
-    protected table: Table;
+    private _element: any;
+    private _type: ElementType;
+    private _cells: Array<Cell>;
+    private _table: Table;
+    private _index: number;
 
     private constructor(cells: Array<Cell>, type: "header"|"body"|"footer" = ElementType.BODY, table?: Table) {
         this.cells = cells;
         this.type = type;
-        this.element = $("<tr>").addClass(Table.classes.row);
+        this.element = $("<tr>").addClass(Table.CLASSES.row);
 
-        if (type === ElementType.FOOTER)
-            this.element.addClass(Table.classes.footerRow);
-        if (type === ElementType.HEADER)
-            this.element.addClass(Table.classes.headerRow);
+        if (type === ElementType.FOOTER) {
+            this.element.addClass(Table.CLASSES.footerRow);
+        }
+
+        if (type === ElementType.HEADER) {
+            this.element.addClass(Table.CLASSES.headerRow);
+        }
+
         this.table = table;
     }
 
     public static from(element: RowElement, table: Table): Row {
-        return new Row(Cell.fromArray(element.cellElements, element.type, table), element.type);
+        return new Row(Cell.fromArray(element.cellElements, element.type, table), element.type, table);
     }
 
     public get(cellIndex: number): Cell|null {
-        return cellIndex >= 0 && cellIndex < this.cells.length ? this.cells[cellIndex] : null;
+        return cellIndex >= 0 && cellIndex < this._cells.length ? this._cells[cellIndex] : null;
     }
 
     public getSize() {
-        return this.cells.length;
-    }
-
-    public static generateEmptyRows(n: number, cells: number, type: "header"|"body"|"footer", table?: Table) {
-        const rowElement: RowElement = {
-            type: ElementType.BODY,
-            cellElements: SimpleCellElement.times(cells)
-        };
-
-        const rows: Array<Row> = [];
-        for (let i = 0; i < n; i++) {
-            rows.push(Row.from(rowElement, table));
-        }
-        return rows;
-    }
-
-    public static fromRaw(raw: {type: "header"|"body"|"footer", data: Array<ICellElement>}): Row {
-        return new Row(Cell.fromArray(SimpleCellElement.fromArray(raw.data, raw.type)), raw.type);
+        return this._cells.length;
     }
 
     public empty() {
-        this.element.empty();
+        this._element.empty();
     }
 
     public render(node: any): any {
         let self: Row = this;
-        this.cells.forEach(function (cell) {
-            self.element.append(cell.element);
+        this._cells.forEach(function (cell, i) {
+            cell.column = i;
+            self._element.append(cell.element);
         });
-        $(node).append(this.element);
+        $(node).append(this._element);
 
         return node;
     }
 
+    set index(i: number) {
+        this._index = i;
+    }
+
+    set element(value: any) {
+        this._element = value;
+    }
+
+    set table(value: Table) {
+        this._table = value;
+    }
+
+    set type(value: ElementType) {
+        this._type = value;
+    }
+
+    set cells(value: Array<Cell>) {
+        this._cells = value;
+    }
+
+    get element(): any {
+        return this._element;
+    }
+
+    get table(): Table {
+        return this._table;
+    }
 }

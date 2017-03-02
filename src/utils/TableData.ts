2f5f1a678ca293323f39243a73082649faa3d2b1
ransomware <mohamed.tahrioui@wheregroup.com>
2017-03-02 16:29:06 +0100

    Add buttonBar field

--- src/utils/TableData.ts
+++ src/utils/TableData.ts
@@ -1,6 +1,8 @@
 import {RowElement} from "../elements/RowElement";
+import {ButtonBar} from "../entities/ButtonBar";
 export interface  TableData {
     header: RowElement,
     footer: RowElement,
-    body: Array<RowElement>
+    body: Array<RowElement>,
+    buttonBar: ButtonBar
 }

f4af9a0087c35ae5cd87b51e1ca7786bcd77fc73
ransomware <mohamed.tahrioui@wheregroup.com>
2017-03-03 21:17:28 +0100

    Add FionaFilter entity

---
+++ src/entities/FionaFilter.ts
@@ -0,0 +1,29 @@
+import {Filter} from "./Filter";
+import {FilterBar} from "../components/FilterBar";
+export class FionaFilter extends Filter {
+
+    public handle(filterBar: FilterBar) {
+        const currentText = filterBar.element.text().trim();
+        const headerName: string = (<string>filterBar.cell.metaData.get("headerName")).toLowerCase();
+
+        if (currentText !== '') {
+            let handlerName = $.inArray(headerName, ["schlagnr", "schlag_nr", "nutz_code"]) ? "handleSchlagNumber" : "handleGenericField";
+            this[handlerName](headerName, currentText);
+        }
+        else
+            this.handleEmpty(headerName)
+    }
+
+    public handleSchlagNumber(headerName: string, currentText: string) {
+        this.equal(headerName, currentText.match(/([0-9]+)/)[1]);
+    }
+
+    public handleGenericField(headerName: string, currentText: string) {
+        this.contains(headerName, currentText);
+    }
+
+    public handleEmpty(headerName: string) {
+        this.notNull(headerName);
+        this.notEqual(headerName, '');
+    }
+}

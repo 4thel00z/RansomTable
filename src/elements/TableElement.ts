a259e67305d9d333ce95bfb1c21717d9cf47da7a
ransomware <mohamed.tahrioui@wheregroup.com>
2017-03-07 11:42:09 +0100

    Add filterBars field to TableElement

--- src/elements/TableElement.ts
+++ src/elements/TableElement.ts
@@ -1,11 +1,17 @@
 import {CellElement} from "./CellElement";
 import {WidgetElement} from "./WidgetElement";
 import {ButtonElement} from "./ButtonBarElement";
+import {Map} from "../utils/Map";
 
-export interface TableElement {
-    footer: Array<CellElement>,
-    header: Array<CellElement>,
-    body: Array<Array<CellElement>>,
+
+export interface TableElement <T> {
+    footer: Array<CellElement>
+    header: Array<CellElement>
+    body: Array<Array<CellElement>>
     widgets: {[column: number]: Array<WidgetElement>}
     buttons: Array<ButtonElement>
+    filterBars?: Map<string>
+    metaData?: {[key: string]: T};
+
+
 }

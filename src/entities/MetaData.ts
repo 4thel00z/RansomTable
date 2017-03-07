62e2d737f01606d056e92bd842d77b801c10a93d
ransomware <mohamed.tahrioui@wheregroup.com>
2017-03-07 11:43:55 +0100

    Add addAll API to MetaData

--- src/entities/MetaData.ts
+++ src/entities/MetaData.ts
@@ -1,5 +1,11 @@
+import {Map} from "../utils/Map";
 export class MetaData<T> {
     private data: {[key: string]: T} = {};
+
+
+    constructor(data: {[p: string]: T} ={}) {
+        this.data = data;
+    }
 
     public has(key: string) {
         return !!this.data[key];
@@ -11,6 +17,12 @@ export class MetaData<T> {
 
     public add(key: string, value: T) {
         this.data[key] = value;
+    }
+
+    public addAll(map: Map<T>) {
+        Object.keys(map).forEach((key: string) => {
+            this.add(key, map[key]);
+        });
     }
 
     public remove(key: string): T|boolean {

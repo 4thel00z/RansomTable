797deb8acf9be83d125410c5038838dd65b82e7f
ransomware <mohamed.tahrioui@wheregroup.com>
2017-03-07 11:42:38 +0100

    Add filterBars and name fields to CellElement

--- src/elements/CellElement.ts
+++ src/elements/CellElement.ts
@@ -1,5 +1,6 @@
 import {ElementType} from "./ElementType";
 import {WidgetBar} from "../components/WidgetBar";
+import {FilterBar} from "../components/FilterBar";
 
 export interface  CellElement {
     content: string ;
@@ -7,4 +8,6 @@ export interface  CellElement {
     classes: Array<string> ;
     type: ElementType;
     widgetBar ?: WidgetBar;
+    name?: string
+    filter?: FilterBar;
 }
